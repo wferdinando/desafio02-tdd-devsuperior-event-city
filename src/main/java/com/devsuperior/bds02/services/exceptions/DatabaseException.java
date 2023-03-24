46d314683ff5442822d7bc77776b23ae9af0da3e
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2023-03-23 22:45:17 -0300

    Correção importação

--- src/main/java/com/devsuperior/bds02/services/exceptions/DatabaseException.java
+++ src/main/java/com/devsuperior/bds02/services/exceptions/DatabaseException.java
@@ -1,4 +1,4 @@
-package com.devsuperior.bds02.controllers.exceptions;
+package com.devsuperior.bds02.services.exceptions;
 
 public class DatabaseException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
